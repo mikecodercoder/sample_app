bd26742051a9aafac8a38343f516ba0dc783908c
Mike <mikecodercoder@gmail.com>
2013-01-23 17:54:31 -0800

    part way through ch 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 SampleApp::Application.routes.draw do
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
   #get "users/new"
   
   # get "static_pages/home"
   root to: 'static_pages#home'
 
-  match '/signup', to: 'users#new' 
-  
   #get "static_pages/help"
   match '/help', to: 'static_pages#help'
   
