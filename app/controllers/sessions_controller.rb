c0c7a5893de6d3c39424872e53373d66f241c762
Mike <mikecodercoder@gmail.com>
2013-01-24 19:01:45 -0800

    most of the way through ch 8 but dropdown broken

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      logger.debug("user.remember_token:" + user.remember_token)
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination' 
+      render 'new'
+    end
   end
+
   def destroy
+    sign_out
+    redirect_to root_url
   end
 end
